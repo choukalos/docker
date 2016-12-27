bf92af1ecc0fa8ebc1f62ce099fb4a80bf3f6f73
Chuck Choukalos <choukalos@gmail.com>
2016-12-27 16:24:38 +0000

    added service.sh script for swarm management & visualization

---
+++ service.sh
@@ -0,0 +1,17 @@
+# Portainer doesn't work unless it's the arm tag
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    portainer/portainer:arm \
+    --swarm
+# Visualize the Swarm
+docker service create \
+    --name=viz \
+    --publish=8080:8080/tcp \
+    --constraint=node.role==manager \
+    --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    alexellis2/visualizer-arm:latest
+
+
