c91538aac023c8c612ade3dd4c78538d604c8486
Chuck Choukalos <choukalos@gmail.com>
2017-03-03 17:21:05 +0000

    added php7-xmlrpc aka simpleXML to support Magento 2 in rpi-php7 and magentopi

--- images/magentopi/start.sh
+++ images/magentopi/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 #
+
+# Check var folder permissions and change to owner apache if not already apache
+varowner=$(stat -c %U /var/www/localhost/htdocs/var)
+if [ $varowner != "apache" ]; then
+  echo "got " . $varowner . " updating var owner to apache"
+  chown -R apache:apache /var/www/localhost/htdocs/var
+fi
+# Now run appropriate command
 if [ "$MODE" == "cron" ];then
   #cron && tail -f /var/log/cron.log
   crond -f -d 0
