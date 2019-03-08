91b2958d2e86999a61c59648e035f82433fe86b2
Chuck Choukalos <choukalos@gmail.com>
2019-03-07 20:32:09 -0600

    updated faas-wappalyzer to use docker secrets and first container run install to setup minio client;  faas-wappalyzer will now return json and save to a minio bucket

--- openfaas/faas-wappalyzer/wappalyzer.sh
+++ openfaas/faas-wappalyzer/wappalyzer.sh
@@ -7,6 +7,20 @@ DATE=`date +%Y%m%d`
 NAME=`echo $URL | sed 's/https*:\/\///g' | sed 's/\/*$//g' | sed 's/\//\_/g' `
 NAME="${DATE}_${NAME}.json"
 OUTPUTOBJECT="local/wappalyzer/${NAME}" 
+MINIOGATEWAY=`cat /var/openfaas/secrets/minio_gateway`
+MINIOACCESS=`cat /var/openfaas/secrets/minio_accesskey`
+MINIOSECRET=`cat /var/openfaas/secrets/minio_secretkey`
+
+
+# Pull secrets and setup NC on first container run
+if [ -e ".mc/config.json" ]
+then
+  rem "do nothing"
+else
+  # initialize mc config
+  `mc config host add local "${MINIOGATEWAY}" "${MINIOACCESS}" "${MINIOSECRET}" `
+fi
+# 
 
 # (does not work) Usee Tee to split stdout so it also pipes to minio
 #eval "wappalyzer ${URL} | tee >( mc pipe ${OUTPUTOBJECT} )"
