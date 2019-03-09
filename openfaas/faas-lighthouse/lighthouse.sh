bfa65b052dcfd8305df4309f9763f1b8804e1861
Chuck Choukalos <choukalos@gmail.com>
2019-03-09 17:45:53 +0000

    Fixed wappalyzer to work with mc; drafted lighthouse and magescan functions

--- openfaas/faas-lighthouse/lighthouse.sh
+++ openfaas/faas-lighthouse/lighthouse.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 # Wrapper script to run lighthouse and output JSON to standard out
-lighthouse $1 --output=json --quiet --chrom-flags='headless'
+URL=$1
+
+DATE=`date +%Y%m%d`
+NAME=`echo $URL | sed 's/https*:\/\///g' | sed 's/\/*$//g' | sed 's/\//\_/g' `
+NAME="${DATE}_${NAME}.json"
+OUTPUTOBJECT="rpi/lighthouse/${NAME}" 
+
+# Pull secrets and setup NC on first container run
+if ! [ -e ".mc/config.json" ]; then
+  # initialize mc config
+  MINIOGATEWAY=`cat /var/openfaas/secrets/minio_gateway`
+  MINIOACCESS=`cat /var/openfaas/secrets/minio_accesskey`
+  MINIOSECRET=`cat /var/openfaas/secrets/minio_secretkey`  
+  `mc config host add rpi "${MINIOGATEWAY}" "${MINIOACCESS}" "${MINIOSECRET}" `
+fi
+# 
+
+`lighthouse "${URL}" --output=json --quiet --chrom-flags='headless' > "/dev/shm/${NAME}" `
+`mc cp "/dev/shm/${NAME}" "${OUTPUTOBJECT}"`
+cat "/dev/shm/${NAME}"
+`rm "/dev/shm/${NAME}"`
 
