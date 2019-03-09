66e172c8f37d1b835e5d34886ee145c3ac8f65ec
Chuck Choukalos <choukalos@gmail.com>
2019-03-09 17:48:59 +0000

    clean up of draft functions with Mc side write

--- openfaas/faas-magescan/magescan.sh
+++ openfaas/faas-magescan/magescan.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 # Wrapper script to run Magescan and output JSON to standard out
-/home/app/magescan scan:all --format=json $1
+URL=$1
+
+DATE=`date +%Y%m%d`
+NAME=`echo $URL | sed 's/https*:\/\///g' | sed 's/\/*$//g' | sed 's/\//\_/g' `
+NAME="${DATE}_${NAME}.json"
+OUTPUTOBJECT="rpi/magescan/${NAME}" 
+
+# Pull secrets and setup NC on first container run
+if ! [ -e ".mc/config.json" ]; then
+  # initialize mc config
+  MINIOGATEWAY=`cat /var/openfaas/secrets/minio_gateway`
+  MINIOACCESS=`cat /var/openfaas/secrets/minio_accesskey`
+  MINIOSECRET=`cat /var/openfaas/secrets/minio_secretkey`  
+  `mc config host add rpi "${MINIOGATEWAY}" "${MINIOACCESS}" "${MINIOSECRET}" `
+fi
+#
+
+`/home/app/magescan scan:all --format=json "${URL}" > "/dev/shm/${NAME}" `
+`mc cp "/dev/shm/${NAME}" "${OUTPUTOBJECT}"`
+cat "/dev/shm/${NAME}"
+`rm "/dev/shm/${NAME}"`
+
+
--- openfaas/faas-wappalyzer/wappalyzer.sh
+++ openfaas/faas-wappalyzer/wappalyzer.sh
@@ -2,22 +2,17 @@
 # Wrapper to fire Wappalyzer and Tee into MC for object store backup
 URL=$1
 
-MC=`which mc`
 DATE=`date +%Y%m%d`
 NAME=`echo $URL | sed 's/https*:\/\///g' | sed 's/\/*$//g' | sed 's/\//\_/g' `
 NAME="${DATE}_${NAME}.json"
 OUTPUTOBJECT="local/wappalyzer/${NAME}" 
-MINIOGATEWAY=`cat /var/openfaas/secrets/minio_gateway`
-MINIOACCESS=`cat /var/openfaas/secrets/minio_accesskey`
-MINIOSECRET=`cat /var/openfaas/secrets/minio_secretkey`
-
 
 # Pull secrets and setup NC on first container run
-if [ -e ".mc/config.json" ]
-then
-  rem "do nothing"
-else
+if ! [ -e ".mc/config.json" ]; then
   # initialize mc config
+  MINIOGATEWAY=`cat /var/openfaas/secrets/minio_gateway`
+  MINIOACCESS=`cat /var/openfaas/secrets/minio_accesskey`
+  MINIOSECRET=`cat /var/openfaas/secrets/minio_secretkey`  
   `mc config host add local "${MINIOGATEWAY}" "${MINIOACCESS}" "${MINIOSECRET}" `
 fi
 # 
@@ -27,7 +22,7 @@ fi
 
 # (works) Do Wappalyzer Scan; saving to shared memory file to copy to Minio and echo back out to stdout for OpenFAAS
 `wappalyzer "${URL}" > "/dev/shm/${NAME}" `
-mc cp "/dev/shm/${NAME}" "${OUTPUTOBJECT}"
-echo "/dev/shm/${NAME}"
-rm "/dev/shm/${NAME}"
+`mc cp "/dev/shm/${NAME}" "${OUTPUTOBJECT}"`
+cat "/dev/shm/${NAME}"
+`rm "/dev/shm/${NAME}"`
 
